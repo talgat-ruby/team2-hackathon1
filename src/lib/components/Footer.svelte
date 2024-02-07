c60df4457c070c27e27ef5f5ac27bdad66a3f0c1
Ilyas Raimkulov <31985607+TheIceEagle@users.noreply.github.com>
2024-02-07 18:20:56 +0600

    Added footer to layout.svelte

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,83 @@
+
+
+
+ <footer class="Rectangle">
+	<button class="Next_Button">
+		<div class="Next_Button_Text">Next Step</div>
+	</button>
+	</footer >
+
+
+<style>
+		/*.Mobile_Body{*/
+    /*    !* Mobile - Step 1 *!*/
+    /*    position: relative;*/
+    /*    width: 375px;*/
+    /*    height: 667px;*/
+    /*    !* BG *!*/
+    /*    background: rgb(239, 245, 255);*/
+		/*}*/
+
+    @media (min-width: 365px) {
+        .Rectangle {
+            position: absolute;
+            width: 375px;
+            height: 72px;
+            left: 0;
+            right: 0;
+            top: 595px;
+            bottom: -595px;
+            box-shadow: 0px 25px 40px -20px rgba(0, 0, 0, 0.1);
+
+            /* White */
+
+            background: rgb(255, 255, 255);
+        }
+        .Next_Button{
+            /* Rectangle */
+
+            cursor: pointer;
+            position: relative;
+
+            width: 97px;
+
+            height: 40px;
+
+            left: 0%;
+
+            right: 0%;
+
+            top: 0%;
+
+            bottom: 0%;
+            /* Denim */
+            background: rgb(2, 41, 89);
+            margin-left: 16px;
+            margin-top: 16px;
+            margin-bottom: 16px;
+            margin-left: 262px;
+            border-radius: 4px;
+        }
+        .Next_Button_Text{
+            /* Next Step */
+
+            position: absolute;
+            width: 65px;
+            height: 12px;
+            left: 16px;
+            right: 16px;
+            top: 12px;
+            bottom: 12px;
+            color: rgb(255, 255, 255);
+            font-family: Ubuntu;
+            font-size: 14px;
+            font-weight: 500;
+            line-height: 16px;
+            letter-spacing: 0px;
+            text-align: left;
+
+        }
+    }
+
+
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script>
     import Sidebar from "$lib/components/Sidebar.svelte";
+		import Footer from '$lib/components/Footer.svelte';
 </script>
 
 <Sidebar />
 
 <slot />
+
+<Footer />
+
+<style>
+
+</style>
