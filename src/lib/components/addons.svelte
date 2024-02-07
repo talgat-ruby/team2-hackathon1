8264e6689b0f1517c1004c244482ad76c6ec24ed
Gulfiya-01 <userbaeva.gm@mail.ru>
2024-02-07 17:54:40 +0600

    divided form into components

---
+++ src/lib/components/addons.svelte
@@ -0,0 +1,97 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+  
+	const dispatch = createEventDispatcher();
+  
+	function goToNextStep() {
+	  dispatch('setpage', { page: 4 });
+	}function goToBackStep() {
+	  dispatch('setpage', { page: 2 });
+	}
+  </script>
+  
+  <div class="stp step-3">
+    <div class="header">
+      <h1 class="title">Pick add-ons</h1>
+      <p class="exp">Add-ons help enhance your gaming experience.</p>
+    </div>
+    <form>
+      <div class="box" data-id="1">
+        <input type="checkbox" id="online" />
+        <div class="description">
+          <label for="online">Online service</label>
+          <small>Access to multiplayer games</small>
+        </div>
+        <p class="price">+$1/mo</p>
+      </div>
+      <div class="box" data-id="2">
+        <input type="checkbox" id="larger" />
+        <div class="description">
+          <label for="larger">Larger storage</label>
+          <small>Extra 1TB of cloud save</small>
+        </div>
+        <p class="price">+$2/mo</p>
+      </div>
+      <div class="box" data-id="3">
+        <input type="checkbox" id="profile" />
+        <div class="description">
+          <label for="profile">Customizable Profile</label>
+          <small>Custom theme on your profile</small>
+        </div>
+        <p class="price">+$2/mo</p>
+      </div>
+    </form>
+    <div class="btns">
+      <button class="prev-stp" type="button" on:click={goToBackStep}>Go Back</button>
+      <button class="next-stp" type="button" on:click={goToNextStep}>Next Step</button>
+    </div>
+  </div>
+  <!-- Step 3 end -->
+  <style>
+    .step-3 {
+
+  width: 80%;
+}
+.step-3 form {
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  justify-content: center;
+  gap: 1.5rem;
+}
+.box {
+  border: 1px solid var(--secondory-color);
+  border-radius: 10px;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+}
+.description {
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+  margin-left: 12px;
+}
+.price {
+  margin-left: auto;
+}
+
+.step-3 form input {
+  accent-color: var(--accent-color);
+  transform: scale(1.3);
+  user-select: none;
+}
+.description label {
+  color: var(--primary-color);
+  font-weight: 700;
+  user-select: none;
+}
+.description small {
+  color: var(--secondary-color);
+  font-weight: 700;
+}
+.price {
+  color: var(--Purplish-blue);
+}
+  </style>
