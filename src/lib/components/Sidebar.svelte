2de87010017c91d19e77b3b8103b39ced8e69328
baglana <102351104+baglana@users.noreply.github.com>
2024-02-07 17:30:25 +0600

    Add active and hover states to circles

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -1,9 +1,17 @@
 <aside>
     <menu>
-        <li><a href="/"><div class="circle">1</div></a></li>
-        <li><a href="/"><div class="circle">2</div></a></li>
-        <li><a href="/"><div class="circle">3</div></a></li>
-        <li><a href="/"><div class="circle">4</div></a></li>
+        <li><a class="active" href="/">
+            <div class="circle">1</div>
+        </a></li>
+        <li><a href="/">
+            <div class="circle">2</div>
+        </a></li>
+        <li><a href="/">
+            <div class="circle">3</div>
+        </a></li>
+        <li><a href="/">
+            <div class="circle">4</div>
+        </a></li>
     </menu>
 </aside>
 
@@ -20,6 +28,18 @@
         place-content: center;
         border: 1px solid var(--sidebar-primary-color);
         border-radius: 50%;
+    }
+
+    .active .circle {
+        background-color: var(--sidebar-accent-color);
+        border-color: var(--sidebar-accent-color);
+        color: var(--primary-color);
+    }
+
+    a:hover .circle {
+        background-color: var(--sidebar-secondary-color);
+        border-color: var(--sidebar-secondary-color);
+        color: var(--sidebar-primary-color);
     }
 
     menu {
