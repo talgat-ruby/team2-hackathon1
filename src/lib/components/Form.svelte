68b07e7ad4f225e4687b5ceb63aa882c3279bfd1
baglana <102351104+baglana@users.noreply.github.com>
2024-02-10 16:36:33 +0600

    Refactor boilerplate and add step1 content

--- src/lib/components/Form.svelte
+++ src/lib/components/Form.svelte
@@ -45,5 +45,4 @@
             required
             type="tel"
     />
-
 </form>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,43 +1,41 @@
 <script>
     // We import our page components (similar to the one above).
-    import Page1 from '$lib/components/Page1.svelte';
-    import Page2 from "$lib/components/Page2.svelte";
+    import Form from "$lib/components/Form.svelte";
+    import Sidebar from "$lib/components/sidebar/Sidebar.svelte";
+    import Footer from "$lib/components/Footer.svelte";
 
-    const pages = [Page1, Page2];
+    // The current step of our form.
+    let step = 0;
 
-    // The current page of our form.
-    let page = 0;
-
-    // The state of all of our pages
-    let pagesState = [];
+    // The state of all of our steps
+    let stepsState = [];
 
     // Our handlers
     function onSubmit(values) {
-        if (page === pages.length - 1) {
-            // On our final page with POST our data somewhere
-            console.log('Submitted data: ', pagesState)
+        if (step === stepsState.length - 1) {
+            // On our final step with POST our data somewhere
+            console.log('Submitted data: ', stepsState)
         } else {
-            // If we're not on the last page, store our data and increase a step
-            pagesState[page] = values;
-            pagesState = pagesState; // Triggering update
-            page += 1;
+            // If we're not on the last step, store our data and increase a step
+            stepsState[step] = values;
+            stepsState = stepsState; // Triggering update
+            step += 1;
         }
     }
 
     function onBack(values) {
-        if (page === 0) return;
-        pagesState[page] = values;
-        pagesState = pagesState; // Triggering update
-        page -= 1;
+        if (step === 0) return;
+        stepsState[step] = values;
+        stepsState = stepsState; // Triggering update
+        step -= 1;
     }
 </script>
 
 <!-- We display the current step here -->
-<svelte:component
-        initialValues={pagesState[page]}
+<Sidebar {step}/>
+<Form
+        initialValues={stepsState[step]}
         {onBack}
         {onSubmit}
-        this={pages[page]}
 />
-
-<!--<slot/>-->
+<Footer/>
