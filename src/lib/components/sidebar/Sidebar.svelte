f2d1fc5b563cbb197d1584c0b4a8aa87eabd572f
baglana <102351104+baglana@users.noreply.github.com>
2024-02-11 17:17:05 +0600

    Fix sidebar mobile layout

--- src/lib/components/sidebar/Sidebar.svelte
+++ src/lib/components/sidebar/Sidebar.svelte
@@ -10,6 +10,10 @@
 
 <style>
     aside {
+        position: absolute;
+        top: 0;
+        z-index: -1;
+        width: 100%;
         padding-top: 2rem;
         min-height: 10.75rem;
         background-image: url("$lib/images/bg-sidebar-mobile.svg");
@@ -17,6 +21,7 @@
         background-size: cover;
 
         @media (min-width: 768px) {
+            position: static;
             background-image: url("$lib/images/bg-sidebar-desktop.svg");
             border-radius: 0.625rem;
             width: 17.125rem;
